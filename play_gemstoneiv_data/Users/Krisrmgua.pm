2ca17da4da6faebf322d612c327226942729aaf5
krisrmgua <krisrmgua@gmail.com>
2017-05-23 15:41:25 -0600

    updating krisrmgua spells

--- play_gemstoneiv_data/Users/Krisrmgua.pm
+++ play_gemstoneiv_data/Users/Krisrmgua.pm
@@ -12,14 +12,14 @@ use vars qw/%GLOBALS %COLORS %HIGHLIGHT_NAMES %HIGHLIGHT_ITEMS %HIGHLIGHT_TEXT %
     account_username		=> 'KRISRMGUA2',
     player_id			=> 'W_KRISRMGUA2_W000',
     player_weapon1		=> 'runestaff',
-    player_weapon2		=> 'rune wristlet',
+    player_weapon2		=> 'rune belt',
     player_cloak		=> 'cloak',
-    player_backpack1		=> 'backpack',
+    player_backpack1		=> 'pack',
     player_backpack2		=> 'cloak',
     );
 
 %HIGHLIGHT_ITEMS = (
-        'my_weapons' => { '(a tanik vultite-capped runestaff)' => "color_gold" },
+        'my_weapons' => { '(an ash vultite-capped runestaff)' => "color_gold" },
     );
 
 %SCRIPTS = (
@@ -27,36 +27,14 @@ use vars qw/%GLOBALS %COLORS %HIGHLIGHT_NAMES %HIGHLIGHT_ITEMS %HIGHLIGHT_TEXT %
         'wear' => [ "clench my $GLOBALS{'player_weapon1'}" ],
         'get' => [ "open my $GLOBALS{'player_backpack1'}", "get {pv1}", "put my {pv1} in my $GLOBALS{'player_backpack1'}", "close my $GLOBALS{'player_backpack1'}"  ],
         'get2' => [ "open my $GLOBALS{'player_backpack2'}", "get {pv1}", "put my {pv1} in my $GLOBALS{'player_backpack2'}", "close my $GLOBALS{'player_backpack2'}"  ],
-        'trolls' => [ "incant 1125" ],
         's' => [ "clench my $GLOBALS{'player_weapon1'}", "get my knife from my shirt", "kneel", "skin {pv1} with my knife", "stand", "put my knife in my shirt", "tap my $GLOBALS{'player_weapon2'}", "loot" ],
         'look' => [ "prepare 116", "cast {pv1}" ],
         'tran' => [ "prepare 225", "cast {pv1}" ],
-        'herbs' => [ "{print}$COLORS{'color_green'}##################### $COLORS{'color_gold'}HERBS$COLORS{'color_green'} ######################$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb1 \t -- $COLORS{'color_green'}Acantha Leaf$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb2 \t -- $COLORS{'color_blue'}Ambrominas Leaf$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb3 \t -- $COLORS{'color_blue'}Wolifrew Lichen$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb4 \t -- $COLORS{'color_blue'}Basal Moss$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb5 \t -- $COLORS{'color_black_on_light_blue'}Ephlox Moss$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb6 \t -- $COLORS{'color_black_on_light_blue'}Aloeas Stem$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb7 \t -- $COLORS{'color_black_on_light_blue'}Pothinir Grass$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb8 \t -- $COLORS{'color_yellow'}Cactacae Spine$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb9 \t -- $COLORS{'color_yellow'}Torban Leaf$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb10\t -- $COLORS{'color_yellow'}Haphip Root$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb11\t -- $COLORS{'color_black_on_yellow'}Calamia Fruit$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb12\t -- $COLORS{'color_black_on_yellow'}Woth Flower$COLORS{'color_normal'}","{print}$COLORS{'color_gold'}\t.cherb13\t -- $COLORS{'color_black_on_yellow'}Sovyn Clove$COLORS{'color_normal'}","{print}$COLORS{'color_green'}##################################################$COLORS{'color_normal'}" ],
-        'cherb1' => [ "prepare 1118", "summon acantha leaf" ],
-        'cherb2' => [ "prepare 1118", "summon ambrominas leaf" ],
-        'cherb3' => [ "prepare 1118", "summon wolifrew lichen" ],
-        'cherb4' => [ "prepare 1118", "summon basal moss" ],
-        'cherb5' => [ "prepare 1118", "summon ephlox moss" ],
-        'cherb6' => [ "prepare 1118", "summon aloeas stem" ],
-        'cherb7' => [ "prepare 1118", "summon pothinir grass" ],
-        'cherb8' => [ "prepare 1118", "summon cactacae spine" ],
-        'cherb9' => [ "prepare 1118", "summon torban leaf" ],
-        'cherb10' => [ "prepare 1118", "summon haphip root" ],
-        'cherb11' => [ "prepare 1118", "summon calamia fruit" ],
-        'cherb12' => [ "prepare 1118", "summon woth flower" ],
-        'cherb13' => [ "prepare 1118", "summon sovyn clove" ],
-        'c' => [ "prepare 1106", "channel {pv1}" ],
-        'v' => [ "incant 1101" ],
-        'buff' => [ "prepare 1130", "cast", "sleep 5", "prepare 1125", "cast", "sleep 20", "prepare 120", "cast", "sleep 5", "prepare 1119", "cast", "sleep 20", "prepare 219", "cast", "sleep 5", "prepare 218", "cast", "sleep 20", "prepare 115", "cast", "sleep 5", "prepare 215", "cast", "sleep 20", "prepare 211", "cast", "sleep 5", "prepare 1109", "cast", "sleep 20", "prepare 107", "cast", "sleep 5", "prepare 103", "cast", "sleep 5", "prepare 202", "cast", "sleep 5", "prepare 102", "cast", "sleep 5", "prepare 101" ],
+        'c' => [ "INCANT 302" ],
+        'buff' => [ "prepare 219", "cast", "sleep 5", "prepare 218", "cast", "sleep 20", "prepare 215", "cast", "sleep 20", "prepare 211", "cast", "sleep 5", "prepare 107", "cast", "sleep 5", "prepare 103", "cast", "sleep 5", "prepare 202", "cast", "sleep 5", "prepare 102", "cast", "sleep 5", "prepare 101", "cast", "sleep 5", "prepare 303", "cast", "sleep 20", "prepare 307", "cast", "sleep 5", "prepare 310", "cast", "sleep 20", "prepare 313", "cast" ],
     );
 
 %TRAVEL = (
-        'empath' => {
-            'ice_tc' => {
-                'move' => [ "n", "e", "ne", "se", "e", "go guild", "go iron door", "go glass door", "go arch", "ask henna about checkin", "go arch", "n", "n", "e", "go annex" ],
-                'discription' => 'Go to Empath Guild',
-            },
-        },
         'locker' => {
             'ice_tc' => {
                 #'move' => [ "e", "e", "s", "go hall", "e", "e", "e", "go cur", "open locker" ],
