07d510819d7df31486fe5b3a879e93dc068492f9
krisrmgua <krisrmgua@gmail.com>
2017-05-23 15:48:42 -0600

    updating bread spell

--- play_gemstoneiv_data/Users/Krisrmgua.pm
+++ play_gemstoneiv_data/Users/Krisrmgua.pm
@@ -31,6 +31,7 @@ use vars qw/%GLOBALS %COLORS %HIGHLIGHT_NAMES %HIGHLIGHT_ITEMS %HIGHLIGHT_TEXT %
         'look' => [ "prepare 116", "cast {pv1}" ],
         'tran' => [ "prepare 225", "cast {pv1}" ],
         'c' => [ "INCANT 302" ],
+        'bread' => [ "prepare 203", "summon peppercorn flatbread", "give flatbread to {pv1}" ],
         'buff' => [ "prepare 219", "cast", "sleep 5", "prepare 218", "cast", "sleep 20", "prepare 215", "cast", "sleep 20", "prepare 211", "cast", "sleep 5", "prepare 107", "cast", "sleep 5", "prepare 103", "cast", "sleep 5", "prepare 202", "cast", "sleep 5", "prepare 102", "cast", "sleep 5", "prepare 101", "cast", "sleep 5", "prepare 303", "cast", "sleep 20", "prepare 307", "cast", "sleep 5", "prepare 310", "cast", "sleep 20", "prepare 313", "cast" ],
     );
 
