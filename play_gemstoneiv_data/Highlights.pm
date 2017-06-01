95ec65f179e0eeffc6c88a7aac7a3a82f500e4ab
krisrmgua <krisrmgua@gmail.com>
2017-06-01 11:32:33 -0600

    adding new emapth

--- play_gemstoneiv_data/Highlights.pm
+++ play_gemstoneiv_data/Highlights.pm
@@ -15,6 +15,7 @@ use vars qw/%GLOBALS %COLORS %HIGHLIGHT_NAMES %HIGHLIGHT_ITEMS %HIGHLIGHT_TEXT %
         'Choss' => "color_pink",
         'Eryah' => "color_pink",
         'Nodr' => "color_pink",
+        'Deza' => "color_pink",
         'Jara' => "color_pink",
         'Missandai' => "color_pink",
         'Imagestar' => "color_pink",
