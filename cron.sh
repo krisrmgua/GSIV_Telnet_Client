495d70e1f3127d14be885373a55b6fef00e02e96
Kristopher Simmons <krisrmgua@gmail.com>
2020-06-10 17:10:02 -0600

    new char

--- cron.sh
+++ cron.sh
@@ -4,5 +4,6 @@
 /website/GSIV_Telnet_Client/play_gemstoneiv_daily.pl Woory > /tmp/Worry.txt
 /website/GSIV_Telnet_Client/play_gemstoneiv_daily.pl Thub > /tmp/Thub.txt
 /website/GSIV_Telnet_Client/play_gemstoneiv_daily.pl Sluf > /tmp/Sluf.txt
+/website/GSIV_Telnet_Client/play_gemstoneiv_daily.pl Pristeen > /tmp/Pristeen.txt
 /website/GSIV_Telnet_Client/play_gemstoneiv_daily.pl Oguo > /tmp/Oguo.txt
 /website/GSIV_Telnet_Client/play_gemstoneiv_daily.pl Krisalan > /tmp/Krisalan.txt
