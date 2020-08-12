49f5d9db3783d1797067830541342dfffc92060b
Kristopher Simmons <krisrmgua@gmail.com>
2020-08-11 21:47:20 -0600

    updated

--- GemstoneIV/play.sh
+++ GemstoneIV/play.sh
@@ -14,11 +14,29 @@ read num
 if [ $num -eq 1 ]
 then
     character=Woory
-    port=8000
+    port=8001
 elif [ $num -eq 2 ]
 then
     character=Sluf
-    port=8001
+    port=8002
+elif [ $num -eq 3 ]
+    character=Pristeen
+    port=8003
+elif [ $num -eq 4 ]
+    character=Aiss
+    port=8004
+elif [ $num -eq 5 ]
+    character=Sorci
+    port=8005
+elif [ $num -eq 6 ]
+    character=Trenus
+    port=8006
+elif [ $num -eq 7 ]
+    character=Wardrob
+    port=8007
+elif [ $num -eq 8 ]
+    character=Loads
+    port=8008
 else
     echo "BAD INPUT"
     exit
