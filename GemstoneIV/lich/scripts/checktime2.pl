d0aaac23e5c78b9c1bd91d7badeb8d27ca45580f
Kris Simmons <krisrmgua@gmail.com>
2020-11-02 15:17:56 -0700

    update

---
+++ GemstoneIV/lich/scripts/checktime2.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use DateTime;
+
+$tm_year += 1900;
+my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time());
+$year += 1900;
+
+#print "$year $mon $mday $hour $min $sec";
+
+my $givenDate1 =  DateTime->new(
+      year       => $year,
+      month      => $mon,
+      day        => $mday,
+      hour       => $hour,
+      minute     => $min,
+      second     => $sec,
+
+      time_zone  => 'America/Boise',
+  );
+my $givenDate2 =  DateTime->new(
+      year       => $year,
+      month      => $mon,
+      day        => $mday,
+      hour       => 23,
+      minute     => 00,
+      second     => 0,
+
+      time_zone  => 'America/Boise',
+  );
+$answer_sec = $givenDate1->subtract_datetime_absolute($givenDate2)->seconds;
+print "$answer_sec";
+#$total = ($answer_sec / 60) / 60;
+#print "\nTOTAL: $total";
--- GemstoneIV/lich/scripts/levelup.pl
+++ GemstoneIV/lich/scripts/levelup.pl
@@ -14,6 +14,8 @@ print "session error: $error" unless ($mail!=-1);
 
 if ("$ARGV[0]" eq "STATUS") {
   $mail->send(-to=>'8013185504@tmomail.net', -subject=>"$ARGV[1]", -body=>"$ARGV[1]");
+}elsif ("$ARGV[0]" eq "WHISPER"){
+  $mail->send(-to=>'8013185504@tmomail.net', -subject=>"WHISPER", -body=>"$ARGV[1]");
 }else{
   $mail->send(-to=>'8013185504@tmomail.net', -subject=>"$ARGV[0] is now level $ARGV[1]", -body=>"$ARGV[0] is now level $ARGV[1]");
 }
