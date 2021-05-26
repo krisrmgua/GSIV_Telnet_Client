40e8ccf06bf7366899525f684cacb69c85c22026
Kris Simmons <krisrmgua@gmail.com>
2021-05-26 17:01:28 -0600

    udpate

--- GemstoneIV/buildxmlfile/build.sh
+++ GemstoneIV/buildxmlfile/build.sh
@@ -12,6 +12,7 @@ else
     echo -e "\t8:\tWide Split"
     echo -e "\t9:\t3 Wide Split"
     echo -e "\t10:\t3 Wide Split no monitor"
+    echo -e "\t11:\tWeb"
     echo -e " "
     echo -e "ENTER SELECTOIN NUMBER: "
 
@@ -191,6 +192,25 @@ then
     cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/fam.xml >> ~/.profanity/kris.xml
     cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/kris_middle.xml >> ~/.profanity/kris.xml
     cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/kris_lap_3_wide_split_no_mon.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/kris_bottom.xml >> ~/.profanity/kris.xml
+elif [ $num -eq 11 ]
+then
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/kris_top.xml > ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/bard.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/empath.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/cleric.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/monk.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/paladin.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/ranger.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/rogue.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/sorcerer.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/warrior.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/wizard.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/enemy.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/mycharecters.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/fam.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/kris_middle.xml >> ~/.profanity/kris.xml
+    cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/kris_web.xml >> ~/.profanity/kris.xml
     cat $HOME/GSIV_Telnet_Client/GemstoneIV/buildxmlfile/kris_bottom.xml >> ~/.profanity/kris.xml
 else
     echo "BAD INPUT"
